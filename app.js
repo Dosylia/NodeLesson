f422666f7cac35e872f0019a05dc8774f75772d0
Dosylia <emma.montbarbon@outlook.fr>
2024-04-19 09:06:24 +0200

    Update Routes

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require ('express');
 const mongoose = require('mongoose');
 
-const app = express();
-
-const Thing = require('./models/Thing');
+const stuffRoutes = require ('./routes/stuff')
 
 mongoose.connect('mongodb+srv://user1:hQJKoAq9ABau0Hhm@cluster0.ic6uwmp.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
 
 app.use(express.json());
 
@@ -18,32 +18,6 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.use('/api/stuff', (req, res, next) => {
-    Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-  app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+  app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
